c3deda14e3c6c4846f0f36a05ca6f42c453cf2bd
wnocchi <wnocchi@student.42.fr>
2023-11-13 16:59:28 +0100

    v6

--- ft_split.c
+++ ft_split.c
@@ -6,78 +6,97 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:49:24 by uilly             #+#    #+#             */
-/*   Updated: 2023/11/11 23:20:24 by wnocchi          ###   ########.fr       */
+/*   Updated: 2023/11/13 16:56:39 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// static int count_words(const char *s, char c)
-// {
-// 	int i;
+static char	*cpy_words(const char *str, char c)
+{
+	size_t	count;
+	char	*ptr;
+	size_t	i;
+	size_t	j;
 
-// 	i = 0;
-// 	while (s[i])
-// 	{
-// 		if
-// 	}
-// }
+	i = 0;
+	j = 0;
+	count = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] && str[i] == c)
+	{
+		i++;
+		j++;
+	}
+	while (str[j] && str[j] != c)
+	{
+		count++;
+		j++;
+	}
+	ptr = ft_substr(str, i, count);
+	return (ptr);
+}
 
-char    **ft_split(char const *s, char c)
+
+static int	count_word(const char *str, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] == c)
+		i++;
+	while (str[i])
+	{
+		if ((str[i] == c && str[i + 1] != c) || (str[i + 1] == '\0'))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+
+char **ft_split(char const *s, char c)
 {
 	char	**ptrs;
 	size_t	i;
-	size_t	j;
-	size_t	k;
 
 	i = 0;
-	j = 0;
-	k = 0;
-	ptrs = malloc(sizeof(char *) * 4 + 1);
-	printf("1");
-	while (s[i])
+	ptrs = malloc(sizeof(char *) * count_word(s, c) + 2);
+	if (!ptrs)
+		return (NULL);
+	while (*s)
 	{
-		printf("2");
-		ptrs[j] = malloc(sizeof(char) * 10);
-		while(s[i] != c)
+		if ((*s != c || *s == '\0'))
 		{
-			ptrs[j][k] = s[i];
-			k++;
-			printf("3");
+			ptrs[i] = cpy_words(s, c);
+			//printf("%s\n", ptrs[i]);
+			//printf("%zu\n", ft_strlen(ptrs[i]));
+			s += ft_strlen(ptrs[i]);
+			i++;
 		}
-		j++;
-		printf("4");
+		s++;
 	}
-	return(ptrs);
+	ptrs[i] = NULL;
+	return (ptrs);
 }
-// bonjour les amis yo
-//  ptrs[1]
-// bonjour
-// les
-// amis
-// yo
+
+
 int main (void)
 {
-	char s1[] = "bonjour les amis yo";
-	int i;
-	int j;
+	char s1[] = "             123456 2 3 4        55 6666  7777 ";
 	char **ptrs;
+	int i = 0;
 	
-	i = 0;
-	j = 0;
-	printf("5");
+	printf("nombre de mots :%d\n", count_word(s1, ' '));
 	ptrs = ft_split(s1, ' ');
-	while (ptrs[i][j])
+	while(ptrs[i])
 	{
-		printf("6");
-		while (ptrs[i][j])
-		{
-			printf("7");
-			printf("%c", ptrs[i][j]);
-			j++;
-		}
+		printf("%s\n", ptrs[i]);
 		i++;
-		printf("8");
 	}
-	
+	// printf("mot copie :%s\n", cpy_words(s1, ' '));
+	//free(ptrs);
 }
